fe2c54cb1a73094d924c88e692936e04be6ffd78
Simon <ndungusimon564@gmail.com>
2025-01-14 17:16:16 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function NavBar() {
   return (
     <nav>
@@ -16,15 +17,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+    <h1>About</h1>
+    </div>
+  );
+  }
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+
+      <Home />
+   
+      <About />
+      
     </div>
   );
 }
